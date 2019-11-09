8a41bd0e75805435faec44ea0ae92047f3d310cb
Ken Goad <goadke@g.cofc.edu>
2019-11-09 01:37:11 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -91,11 +91,7 @@ def changing_alan
           :known_for => "Unix",
           :languages => ["C"]
         }
-     }
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
-     
-     
+
 end
 
 def adding_to_dennis
