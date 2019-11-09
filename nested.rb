489f49f70f8dc459535a610b58aa45f9d928a5fc
Ken Goad <goadke@g.cofc.edu>
2019-11-09 01:51:14 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -96,5 +96,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
  		}
- 		programmer_hash[:dennis_ritchie][:languages.add  
+ 		programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 end
