3aa06ffdf75215e4e609fd4473709696da00df17
Ken Goad <goadke@g.cofc.edu>
2019-11-09 01:43:50 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -41,7 +41,7 @@ def dennis_ritchies_language
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
+programmer_hash.push 
 # :yukihiro_matsumoto => {
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
